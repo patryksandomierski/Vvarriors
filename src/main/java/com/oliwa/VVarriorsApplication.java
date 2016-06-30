283650ba0cdff828caf31ba8e5009de24495a99c
patryk_sandomierski <patryksandomierski@gmail.com>
2016-06-30 12:09:14 +0200

    basic project structure

---
+++ src/main/java/com/oliwa/VVarriorsApplication.java
@@ -0,0 +1,12 @@
+package com.oliwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VVarriorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VVarriorsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oliwa/VVarriorsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.oliwa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = VVarriorsApplication.class)
+@WebAppConfiguration
+public class VVarriorsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
