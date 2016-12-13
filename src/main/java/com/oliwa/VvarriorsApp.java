332d979ebd75d54d0c470f8d7d80de67df3d6bb7
Patryk Sandomierski <Patryk Sandomierski>
2016-12-13 22:44:14 +0100

    i wont give up renaming zis

--- src/main/java/com/oliwa/VvarriorsApp.java
+++ src/main/java/com/oliwa/VvarriorsApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VvarriorsApplication {
+public class VvarriorsApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VvarriorsApplication.class, args);
+		SpringApplication.run(VvarriorsApp.class, args);
 	}
 }
--- src/test/java/com/oliwa/VvarriorsAppTests.java
+++ src/test/java/com/oliwa/VvarriorsAppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class VvarriorsApplicationTests {
+public class VvarriorsAppTests {
 
 	@Test
 	public void contextLoads() {
