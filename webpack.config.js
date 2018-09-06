1764e30ecd787ed2d51bc805485e9b928f78916a
Patryk Sandomierski <psandomierski@pl.sii.eu>
2018-09-06 12:17:19 +0200

    Update dependencies (eg WebPack 1.* to 3.*)

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,16 @@ module.exports = {
     entry: './src/main/resources/public/js/app.js',
     devtool: 'sourcemaps',
     cache: true,
-    debug: true,
     output: {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
-                test: path.join(__dirname, '.'),
+                test: /\.(js|jsx)$/,
                 exclude: /(node_modules)/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
